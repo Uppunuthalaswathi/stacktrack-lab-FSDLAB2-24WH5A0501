033139fc01e86f5bb5cf6dbd5d7bb51ccfbd4e7a
kanthi <23wh1a0527@bvrithyderabad.edu.in>
2026-04-10 13:10:25 +0530

    home page is modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 // TODO: Replace placeholder values with actual student and lab identifiers
-const STUDENT_ID = 'STUDENT_ID_PLACEHOLDER';
-const LAB_ID = 'LAB_ID_PLACEHOLDER';
+const STUDENT_ID = '24WH5A0501';
+const LAB_ID = 'FSDLAB2';
 
 function Home() {
   return (
     <div>
+      <h2>Home page for TaskList</h2>
       <h1>Stack Track Lab</h1>
       <p>Student ID: {STUDENT_ID}</p>
       <p>Lab ID: {LAB_ID}</p>
