32032429f9a9026e4f9394fe00aa297869f74679
UppunuthalaSwathi <24wh5a0501@bvrithyderabad.edu.in>
2026-04-10 21:32:09 +0530

    Added QuestionPage to render QuestionComponent

--- src/pages/QuestionPage.js
+++ src/pages/QuestionPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import QuestionComponent from '../components/question/QuestionComponent';
 
 function QuestionPage() {
   return (
     <div>
       <h1>Question Page</h1>
+      <p>
+        <Link to="/">Back to Home</Link>
+      </p>
       <QuestionComponent />
     </div>
   );
 }
-
 export default QuestionPage;
